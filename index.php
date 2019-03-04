141625d3542c96ae8df1f325997722876f154380
Matthew James <Quacky2200@users.noreply.github.com>
2019-03-04 05:59:20 +0000

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>	
-<title>#39 - Running out of Power</title>
-<link rel='stylesheet' href='css/style.css'>
-<script src='js/require.js'></script>
-</head>
-<body class='doctorwho'>
-</body>
-</html>
